d2448d4b9e59e52d3893e7dee1779bb802637d26
Brian Peasley <brpeas@outlook.com>
2018-08-20 21:48:42 -0700

    Error handling in case of connecion loss

--- PiApp/lock.py
+++ PiApp/lock.py
@@ -5,21 +5,20 @@ from azure.storage.queue import QueueService
 from servosix import ServoSix
 
 ss = ServoSix()
-queue_service = QueueService(account_name=sys.argv[1], account_key=sys.argv[2])
-if queue_service is None:
-	print('Error creating queue service')
-	exit()
-
-print('Listening to azure storage queue')
 while True:
-	messages = queue_service.get_messages('locks')
-	for message in messages:
-		msg = base64.b64decode(message.content)
-		queue_service.delete_message('locks', message.id, message.pop_receipt)
+	try:
+		queue_service = QueueService(account_name=sys.argv[1], account_key=sys.argv[2])
+		messages = queue_service.get_messages('locks')
+		for message in messages:
+			msg = base64.b64decode(message.content)
+			queue_service.delete_message('locks', message.id, message.pop_receipt)
 
-		angle = int(msg)
-		print('Setting servo 1 to angle ' + msg)
-		ss.set_servo(1, angle)
+			angle = int(msg)
+			print('Setting servo 1 to angle ' + msg)
+			ss.set_servo(1, angle)
+	except:
+		print('Error')
+		time.sleep(3)
 
 	time.sleep(3)
 
